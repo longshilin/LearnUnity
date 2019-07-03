a02c0423b78f73235be80321af736de9eb7b79ed
Longshilin <583297550@qq.com>
2019-07-03 21:58:36 +0800

    update

---
+++ Assets/Scripts/03 ControllingGameObjectsComponents/WaypointManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public Transform[] waypoints;
+    
+    void Start() 
+    {
+        waypoints = new Transform[transform.childCount];
+        int i = 0;
+        
+        foreach (Transform t in transform)
+        {
+            waypoints[i++] = t;
+        }
+
+        // print all waypoints gameobject name
+        foreach (var e in waypoints)
+        {
+            print(e.name);
+        }
+
+    }
+}
---
+++ Assets/Scripts/CreateDestroyObjects/CreateDestroyObjects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDestroyObjects : MonoBehaviour
+{
+    public GameObject enemy;
+
+    void Start() {
+        for (int i = 0; i < 5; i++) {
+            Instantiate(enemy);
+        }
+    }
+}
